7e7c0c9880def5ab6215c230e9617a186d1d3052
kushbharti <try.kushbharti@gmail.com>
2025-06-16 20:58:18 +0530

    Html Pages

--- website/settings.py
+++ website/settings.py
@@ -121,6 +121,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILE_DIRS = [
-    BASE_DIR,'static',
+STATICFILES_DIRS = [
+    BASE_DIR /'static',
 ]
--- website/urls.py
+++ website/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.login, name='login'),
+    path('register/',views.register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL ,document_root = settings.STATICFILES_DIRS[0])
