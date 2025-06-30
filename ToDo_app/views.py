3e0342c9a0cd5b0b0625894e5de2430f9d4fa355
kushbharti <try.kushbharti@gmail.com>
2025-06-30 09:35:44 +0530

    added commit

--- ToDo_app/views.py
+++ ToDo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -9,10 +10,15 @@ def home(request):
         title = request.POST['title']
         desc = request.POST['desc']
         Todo.objects.create(todo_title = title, todo_desc = desc)
-        
+ 
         return redirect('todo')
     todos = Todo.objects.all()
-    return render(request,'todo_home.html',{'todos':todos})
+    
+    paginator = Paginator(todos,4)
+    page_no = request.GET.get('page')
+    page_obj = paginator.get_page(page_no)
+    total_page = page_obj.paginator.num_pages
+    return render(request,'todo_home.html',{'todos':todos,'page_obj':page_obj,'total_page':[n+1 for n in range(total_page)]})
 
 def update_todo(request,slug):
     todo = Todo.objects.get(slug=slug)
